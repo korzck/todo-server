3dacbb19f8988fecfdfce56b19fa46b2c1ba4656
korzck <konstantin.koretskiy@gmail.com>
2023-04-17 22:06:30 +0300

    added cors for tauri

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"todo-server/initializers"
 	"todo-server/models"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,7 +15,10 @@ func init() {
 
 func main() {
 	r := gin.Default()
-	// r.Use(cors.Default())
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost:1420"},
+		AllowHeaders: []string{"Origin", "Content-Type", "Accept"},
+	}))
 	// config := cors.DefaultConfig()
 	// config.AllowOrigins = []string{"http://localhost:3000/todos"}
 	// config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE"}
@@ -25,23 +29,5 @@ func main() {
 	r.GET("/todos", models.GetTodo)
 	r.POST("/todos/delete", models.DelTodo)
 	// r := gin.Default()
-	r.Use(CORSMiddleware())
 	r.Run()
-}
-
-func CORSMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		//Access-Control-Allow-Origin: http://www.example.com
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(200)
-		} else {
-			c.Next()
-		}
-	}
 }
